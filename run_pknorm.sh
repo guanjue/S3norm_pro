9080c08f3668f7f24cb06b5bfbb57eb659614137
Universe <universe@MacBook-Air.local>
2018-04-25 02:25:30 -0400

    add common pknorm

--- run_pknorm.sh
+++ run_pknorm.sh
@@ -1,3 +1,6 @@
-script_folder='/Users/universe/Documents/pknorm_test/pknorm_scripts/'
+script_folder='/storage/home/gzx103/group/software/PKnorm/pknorm_scripts/'
 
 time python $script_folder'pknorm_0326.py' -r reference_signal_track.txt -t target_signal_track.txt -m 1 -i 2 -f 0.05 -n 5000 -l 1000 -a 100 -b 0 -s $script_folder
+
+
+time python $script_folder'pknorm_0326.py' -r ERY_fl.ctcfrep.100162.bamtobed5endintersect.signal -t ERY_fl.ctcfrep.100163.bamtobed5endintersect.signal -m 1 -i 2 -f 0.05 -n 100000 -l 10000 -a 100000 -b 0 -s $script_folder
