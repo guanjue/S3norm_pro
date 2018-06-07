8d466a3c662e70f56cbac88e1ccba810ba1644b6
Universe <universe@MacBook-Air.local>
2018-06-07 14:44:22 -0400

    compare all 5

---
+++ pknorm_scripts/MAnorm_0607.R
@@ -0,0 +1,96 @@
+#library(robustreg)
+library(MASS)
+library(affy)
+
+get_nbp = function(sig){
+	thesh = 0
+	sig_non0 = sig[sig>thesh]
+	sig_mean = mean(sig_non0)
+	sig_var = var(sig_non0)
+	print(paste('check signal track overdispersion in background regions, var/mean=', toString(round(sig_var/sig_mean, digits=3)) ))
+	sig_prob = sig_mean / sig_var
+	if (sig_prob<0.1){
+		print('use lower bound for nbp prob')
+		sig_prob = 0.1
+	}
+	if (sig_prob>=0.9){
+		print('use upper bound for nbp prob')
+		sig_prob = 0.9
+	}
+	sig_size = sig_mean * sig_prob / (1-sig_prob)
+	nb_pval = apply(as.matrix(sig_non0), MARGIN=1, function(x) pnbinom(x[1], sig_size, sig_prob, lower.tail=FALSE) )
+	### 2nd round
+	sig_non0_bg = sig_non0[nb_pval>=0.001]
+	sig_non0_bg_mean = mean(sig_non0_bg)
+	sig_non0_bg_var = var(sig_non0_bg)
+	print(paste('2nd round check signal track overdispersion in background regions, var/mean=', toString(round(sig_var/sig_mean, digits=3)) ))
+	sig_prob_2nd = sig_non0_bg_mean / sig_non0_bg_var
+	if (sig_prob_2nd<0.1){
+		print('use lower bound for nbp prob')
+		sig_prob_2nd = 0.1
+	}
+	if (sig_prob_2nd>=0.9){
+		print('use upper bound for nbp prob')
+		sig_prob_2nd = 0.9
+	}
+	sig_size_2nd = sig_non0_bg_mean * sig_prob_2nd / (1-sig_prob_2nd)
+	nb_pval_2nd = apply(as.matrix(sig), MARGIN=1, function(x) pnbinom(x[1], sig_size_2nd, sig_prob_2nd, lower.tail=FALSE) )
+	return(nb_pval_2nd)
+}
+
+
+args = commandArgs(trailingOnly=TRUE)
+input_sig1 = args[1]
+input_sig2 = args[2]
+output = args[3]
+
+small_num = 0.01
+random_sample_num = 1000000
+upperlim = 1000
+lowerlim = 0
+ct = unlist(strsplit(input_sig2, "[.]"))[1]
+
+sig1 = scan(input_sig1)
+sig2 = scan(input_sig2)
+totalmean_sf = sum(sig1) / sum(sig2)
+sig3 = (sig2) * totalmean_sf #- small_num
+
+
+sig1_binary = get_nbp(sig1) <= 0.001
+sig2_binary = get_nbp(sig2) <= 0.001
+
+peak_binary_pk = as.logical(sig1_binary * sig2_binary) 
+peak_binary = peak_binary_pk & (sig1 != sig1[1]) & (sig2 != sig2[1])
+
+bg_binary_bg = as.logical((sig1_binary + sig2_binary)==0)
+bg_binary = bg_binary_bg & (sig1 != sig1[1]) & (sig2 != sig2[1]) 
+
+common_peak_count_read1 = sig1[peak_binary]
+common_peak_count_read2 = sig2[peak_binary]
+
+
+M=log2((common_peak_count_read2+small_num)/(common_peak_count_read1+small_num))
+A=0.5*log2((common_peak_count_read2+small_num)*(common_peak_count_read1+small_num))
+M = as.matrix(M)
+A = as.matrix(A)
+
+linear=lm(M~A)$coefficients
+#b=lm(M~A)$coefficients
+#b=robustRegBS(M,A,beta=linear)
+b=rlm(M~A)$coefficients
+
+cat("M = b[1] + b[2] * A\n")
+
+log2_allregion_count_read1 = log2(sig1 + small_num)
+log2_allregion_count_read2 = log2(sig2 + small_num)
+log2_allregion_count_read2_rescaled = (2-b[2])*log2_allregion_count_read2/(2+b[2]) - 2*b[1]/(2+b[2]);
+sig2_rescaled = 2^log2_allregion_count_read2_rescaled - small_num
+
+#write.table(info, paste(output, '.MA.norm.info.txt', sep=''),sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
+write.table(sig2_rescaled, paste(output,".manorm.txt", sep=''),sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
+write.table(sig3, paste(output,".trnorm.txt", sep=''),sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
+
+
+
+
+
--- pknorm_scripts/cmpare_rep_frip_snr_index.R
+++ pknorm_scripts/cmpare_rep_frip_snr_index.R
@@ -7,17 +7,21 @@ args = commandArgs(trailingOnly=TRUE)
 sig1_raw = args[1]
 sig2_raw = args[2]
 
-sig2_TRnorm = args[3]
+sig1_TRnorm = args[3]
+sig2_TRnorm = args[4]
 
-sig2_MAnorm = args[4]
+sig1_MAnorm = args[5]
+sig2_MAnorm = args[6]
 
-sig2_QTnorm = args[5]
+sig1_QTnorm = args[7]
+sig2_QTnorm = args[8]
 
-sig2_PKnorm = args[6]
+sig1_PKnorm = args[9]
+sig2_PKnorm = args[10]
 
-output_name = args[7]
-fdr_thresh = as.numeric(args[8])
-
+output_name = args[11]
+fdr_thresh = as.numeric(args[12])
+method=args[13]
 ################################################
 nbp_2r = function(sig, p_lim_1r, output_name){
 	### get mean & var
@@ -66,6 +70,18 @@ nbp_2r = function(sig, p_lim_1r, output_name){
 }
 
 
+zp = function(sig){
+	### get mean & var
+	thesh = -1
+	sig_non0 = sig[sig>=thesh]
+	sig_mean = mean(sig_non0)
+	sig_sd = sd(sig_non0)
+	z = (sig - sig_mean)/sig_sd
+	zp = pnorm(-z)
+	return(zp)
+}
+
+
 jaccard_index = function(pk_binary_1, pk_binary_2){
 	overlap = (pk_binary_1 * pk_binary_2)==1
 	pk_overlap_num = sum(overlap)
@@ -104,48 +120,69 @@ sig2_ma = scan(sig2_MAnorm)
 sig2_qt = scan(sig2_QTnorm)
 sig2_pk = scan(sig2_PKnorm)
 
-sig_matrix = as.matrix(cbind(sig1, sig2_r, sig2_tr, sig2_ma, sig2_qt, sig2_pk))
-sig_matrix_colnames = c('sig1', 'sig2_r', 'sig2_tr', 'sig2_ma', 'sig2_qt', 'sig2_pk')
+sig_matrix = as.matrix(cbind(sig1_r, sig1_tr, sig1_ma, sig1_qt, sig1_pk, sig2_r, sig2_tr, sig2_ma, sig2_qt, sig2_pk))
+sig_matrix_colnames = c('sig1_r', 'sig1_tr', 'sig1_ma', 'sig1_qt', 'sig1_pk', 'sig2_r', 'sig2_tr', 'sig2_ma', 'sig2_qt', 'sig2_pk')
 
 ### get nbp
-sig_matrix_nbp = apply(sig_matrix, 2, function(x) p.adjust(nbp_2r(x, 0.001, paste(sig1_raw, '.nbp_2r.txt', sep='')), method='fdr'))
+if (method == 'nbp'){
+	sig_matrix_p = apply(sig_matrix, 2, function(x) p.adjust(nbp_2r(x, 0.001, paste(sig1_raw, '.nbp_2r.txt', sep='')), method='fdr'))
+} else if (method == 'p'){
+	sig_matrix_p = apply(sig_matrix, 2, function(x) p.adjust(x, method='fdr'))	
+} else if (method == 'neglog10p'){
+	sig_matrix_p = apply(sig_matrix, 2, function(x) p.adjust(10^(-x), method='fdr'))
+} else if (method == 'zp'){
+	sig_matrix_p = apply(sig_matrix, 2, function(x) p.adjust(zp(x), method='fdr'))
+}
 
-frip_all = c()
-snr_all = c()
-pk_num_all = c()
+frip1_all = c()
+snr1_all = c()
+pk_num1_all = c()
+frip2_all = c()
+snr2_all = c()
+pk_num2_all = c()
 ari_all = c()
 ji_all = c()
 
 frip_common_all = c()
 
-sig1_pk_id = sig_matrix_nbp[,1]<fdr_thresh
+sig1_pk_id = sig_matrix_p[,1]<fdr_thresh
 sig1 = sig_matrix[,1]
 
-for ( i in c(1:dim(sig_matrix)[2])){
+for ( i in c(1:(dim(sig_matrix)[2])/2)){
 	print(sig_matrix_colnames[i])
 	### get info
-	nbp_tmp = sig_matrix_nbp[,i]
-	sig_tmp = sig_matrix[,i]
-	pk_id_tmp = nbp_tmp<fdr_thresh
-	bg_id_tmp = nbp_tmp>=fdr_thresh
-	frip_tmp = sum(sig_tmp[pk_id_tmp]) / sum(sig_tmp)
-	snr_tmp = mean(sig_tmp[pk_id_tmp]) / mean(sig_tmp[bg_id_tmp])
-	pk_num_tmp = sum(pk_id_tmp)
-	ari_tmp = adjustedRandIndex(sig1_pk_id, pk_id_tmp)
-	ji_tmp = jaccard_index(sig1_pk_id, pk_id_tmp)
-	frip_common_tmp = frip_common(sig1, sig1_pk_id, sig_tmp, pk_id_tmp)
+	nbp1_tmp = sig_matrix_p[,i]
+	sig1_tmp = sig_matrix[,i]
+	nbp2_tmp = sig_matrix_p[,i+dim(sig_matrix)[2])/2]
+	sig2_tmp = sig_matrix[,i+dim(sig_matrix)[2])/2]
+	pk1_id_tmp = nbp1_tmp<fdr_thresh
+	bg1_id_tmp = nbp1_tmp>=fdr_thresh
+	pk2_id_tmp = nbp2_tmp<fdr_thresh
+	bg2_id_tmp = nbp2_tmp>=fdr_thresh
+	frip1_tmp = sum(sig1_tmp[pk1_id_tmp]) / sum(sig1_tmp)
+	snr1_tmp = mean(sig1_tmp[pk1_id_tmp]) / mean(sig1_tmp[bg1_id_tmp])
+	pk_num1_tmp = sum(pk1_id_tmp)
+	frip2_tmp = sum(sig2_tmp[pk2_id_tmp]) / sum(sig2_tmp)
+	snr2_tmp = mean(sig2_tmp[pk2_id_tmp]) / mean(sig2_tmp[bg2_id_tmp])
+	pk_num2_tmp = sum(pk2_id_tmp)
+	ari_tmp = adjustedRandIndex(pk1_id_tmp, pk2_id_tmp)
+	ji_tmp = jaccard_index(pk1_id_tmp, pk2_id_tmp)
+	frip_common_tmp = frip_common(sig1_tmp, pk1_id_tmp, sig2_tmp, pk2_id_tmp)
 	### append(info)
-	frip_all[i] = frip_tmp
-	snr_all[i] = snr_tmp
-	pk_num_all[i] = pk_num_tmp
+	frip1_all[i] = frip1_tmp
+	snr1_all[i] = snr1_tmp
+	pk_num1_all[i] = pk_num1_tmp
+	frip2_all[i] = frip2_tmp
+	snr2_all[i] = snr2_tmp
+	pk_num2_all[i] = pk_num2_tmp
 	ari_all[i] = ari_tmp
 	ji_all[i] = ji_tmp
 	frip_common_all = cbind(frip_common_all, frip_common_tmp)
 }
 
 
-info_matrix = cbind(frip_all, snr_all, pk_num_all, ari_all, ji_all, t(frip_common_all))
-colnames(info_matrix) = c('frip', 'snr', 'pk_num', 'ari', 'ji', 'frip_cpk', 'frip_cpk_ref', 'frip_cpk_tar')
+info_matrix = cbind(frip1_all, snr1_all, pk_num1_all, frip2_all, snr2_all, pk_num2_all, ari_all, ji_all, t(frip_common_all))
+colnames(info_matrix) = c('frip1', 'snr1', 'pk_num1', 'frip2', 'snr2', 'pk_num2', 'ari', 'ji', 'frip_cpk', 'frip_cpk_ref', 'frip_cpk_tar')
 rownames(info_matrix) = sig_matrix_colnames
 
 write.table(info_matrix, output_name, quote=FALSE, col.names=TRUE, row.names=TRUE, sep='\t')
