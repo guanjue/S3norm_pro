aa80b207b97b94db8e8ca3e7fbe785b4c8fe5fcb
Universe <universe@MacBook-Air.local>
2018-07-28 01:06:46 -0400

    adjust z score

--- pknorm_scripts/Znorm_0607.R
+++ pknorm_scripts/Znorm_0607.R
@@ -9,9 +9,9 @@ lowerlim = 0
 ct = unlist(strsplit(input_sig1, "[.]"))[1]
 
 sig1 = scan(input_sig1)
-sig1_log2 = log2(sig1+small_num)
-sig1_z = (sig1_log2 - mean(sig1_log2))/sd(sig1_log2)
-
+#sig1_log2 = log2(sig1+small_num)
+#sig1_z = (sig1_log2 - mean(sig1_log2))/sd(sig1_log2)
+sig1_z = (sig1 - mean(sig1))/sd(sig1)
 
 write.table(sig1_z, paste(output,".znorm.txt", sep=''),sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE)
 
