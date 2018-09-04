974a2eecaf63f4ec1a1377f3f3b93174b0327e29
Universe <universe@MacBook-Air.local>
2018-09-04 01:01:32 -0400

    fdr lim list pk

--- pknorm_scripts/dif_fdr_pk.R
+++ pknorm_scripts/dif_fdr_pk.R
@@ -5,7 +5,7 @@ output_bed = args[2]
 
 data = read.table(input_bed, header=FALSE, sep='\t')
 
-data_p = data[,4]
+data_p = 10^(-data[,4])
 data_p_fdr = p.adjust(data[,4], 'fdr')
 
 thresh_list = c(0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001)
